540f8721ab11024b02e88a397d81e794031bc301
kirster <kir.sazonov@gmail.com>
2017-11-09 19:03:55 +0300

    create User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171109155948) do
+ActiveRecord::Schema.define(version: 20171109160235) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
@@ -28,6 +28,12 @@ ActiveRecord::Schema.define(version: 20171109155948) do
   create_table "tests", force: :cascade do |t|
     t.string "title", null: false
     t.integer "level", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
