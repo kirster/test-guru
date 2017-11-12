aafa4fc085c1a139c0257165f1a31e16d2cbf78d
kirster <kir.sazonov@gmail.com>
2017-11-12 11:23:27 +0300

    add initial data for models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171109160523) do
+ActiveRecord::Schema.define(version: 20171111124807) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
@@ -36,6 +36,8 @@ ActiveRecord::Schema.define(version: 20171109160523) do
     t.integer "level", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "category_id"
+    t.index ["category_id"], name: "index_tests_on_category_id"
   end
 
   create_table "users", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,77 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times { User.create(email: Faker::Internet.email) }
+
+categories = %w[Git Backend Algorithms]
+categories.each { |category| Category.create(title: category) }
+
+tests = [
+  ['Git basics', 1, 1],
+  ['Git branching', 2, 1],
+  ['Ruby beginner', 1, 2],
+  ['Ruby OOP', 2, 2],
+  ['SQL in Rails', 3, 2],
+  ['Algo basics', 1, 3],
+  ['Searching', 2, 3]
+]
+
+tests.each do |title, level, category|
+  Test.create(title: title, level: level, category_id: category)
+end
+
+questions = [ 'How do you start Git repository?',
+              'How do you determine the current state of the project?',
+              'Command to put changed files to staging condition?',
+              'How to show list of all the commits that have been made?',
+              'How to create a new branch?',
+              'How to show list of all branches?',
+              'How to switch to master branch?',
+              'Command to merge one branch to another?',
+              "What is the datatype of 'one'?",
+              "What does 'self' mean?",
+              'What is the keyword for creating a new method?',
+              'What type is :name?',
+              'What is an object?',
+              'What is a command for creating an object?',
+              'What is the type of variable @@name?',
+              'What is the keyword to define a ruby module?',
+              'What SQL is generated to create a table in the database?',
+              'What SQL is used to delete database?',
+              'What attribute does find method use?',
+              'What is the function that calls itself?',
+              'Convert 1110 base-2 to the corresponding base-ten number',
+              'Convert 63933 base-ten to the corresponding hexadecimal number',
+              'What is n in O(n)?',
+              'What is the average Big O performance of quicksort algorithm?',
+              'What is the best Big O performance of bubble sort?',
+              'What is the worst Big O performance of insertion sort?'
+            ]
+
+questions.each { |question| Question.create(body: question) }
+
+answers =[{ body: 'git init', correct: true }, { body: 'git start' }, { body: 'git new' },
+          { body: 'git log'}, { body: 'git status', correct: true}, { body: 'git init' },
+          { body: 'git commit'}, { body: 'git checkout' }, { body: 'git add', correct: true },
+          { body: 'git commit'}, { body: 'git add', correct: true }, { body: 'git status'},
+          { body: 'git new branch'}, { body: 'git branch <name>', correct: true }, { body: 'git -b'},
+          { body: 'git branches'}, { body: 'git branch', correct: true }, { body: 'git log'},
+          { body: 'git master'}, { body: 'git checkout master', correct: true }, { body: 'git HEAD master'},
+          { body: 'git merge <branch_name>', correct: true }, { body: 'git merge all'}, { body: 'git merge HEAD'},
+          { body: 'integer'}, { body: 'float'}, { body: 'string', correct: true },
+          { body: 'current object', correct: true }, { body: 'integer class method'}, { body: 'string class method' },
+          { body: 'function'}, { body: 'def', correct: true }, { body: 'void' },
+          { body: 'decimal' }, { body: 'string' }, { body: 'symbol', correct: true },
+          { body: 'class' }, { body: 'instance of class', correct: true }, { body: 'module' },
+          { body: 'void' }, { body: 'new', correct: true }, { body: 'object_id' },
+          { body: 'global' }, { body: 'class', correct: true }, { body: 'instance' },
+          { body: 'module', correct: true }, { body: 'class' }, { body: 'mixin' },
+          { body: 'create database <name>' }, { body: 'create table <name>', correct: true }, { body: 'insert into <name>' },
+          { body: 'drop database <name>', correct: true }, { body: 'delete database <name>' }, { body: 'delete db <name>' },
+          { body: 'created_at' }, { body: 'id', correct: true }, { body: 'updated_at' },
+          { body: 'recursion', correct: true }, { body: 'bubble sort' }, { body: 'binary tree' },
+          { body: '10' }, { body: '14', correct: true }, { body: '12' },
+          { body: 'DDF' }, { body: 'F9BD', correct: true }, { body: 'FF' },
+          { body: 'number of operations', correct: true }, { body: 'estimated time in seconds' }, { body: 'argument' },
+          { body: 'n*log(n)', correct: true }, { body: 'n' }, { body: 'n**2' },
+          { body: 'n*log(n)' }, { body: 'n', correct: true }, { body: 'n**2' },
+          { body: 'n*log(n)' }, { body: 'n' }, { body: 'n**2', correct: true } ]   
+
+Answer.create(answers)
