d6e7e8fc6c46a1186eb05ca24ff9578e8da57a29
kirster <kir.sazonov@gmail.com>
2017-11-12 11:25:53 +0300

    add tests_by_category class method to test model

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,7 @@
 class Test < ApplicationRecord
+  belongs_to :category
+
+  def self.tests_by_category(category)
+    Test.joins(:category).where(categories: { title: category }).order(title: :desc)
+  end
 end
