363cb3f8c2a4608939e065b5ae8655eb06774989
kirster <kir.sazonov@gmail.com>
2017-11-14 19:37:38 +0300

    add unique option to users_tests table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171113124540) do
+ActiveRecord::Schema.define(version: 20171114163319) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
@@ -43,7 +43,7 @@ ActiveRecord::Schema.define(version: 20171113124540) do
   create_table "tests_users", id: false, force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "test_id", null: false
-    t.index ["user_id", "test_id"], name: "index_tests_users_on_user_id_and_test_id"
+    t.index ["user_id", "test_id"], name: "index_tests_users_on_user_id_and_test_id", unique: true
   end
 
   create_table "users", force: :cascade do |t|
