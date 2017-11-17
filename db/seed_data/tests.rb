c5aa89e6aa2eb2ac95c498b99a6012e35c4d53d7
kirster <kir.sazonov@gmail.com>
2017-11-17 16:14:56 +0300

    change tests seed_data

--- db/seed_data/tests.rb
+++ db/seed_data/tests.rb
@@ -1,11 +1,11 @@
 tests = [
   ['Git basics', 1, 1, 1],
   ['Git branching', 2, 1, 2],
-  ['Ruby beginner', 1, 2, 3],
-  ['Ruby OOP', 2, 2, 4],
-  ['SQL in Rails', 3, 2, 5],
-  ['Algo basics', 1, 3, 6],
-  ['Searching', 2, 3, 7]
+  ['Ruby beginner', 0, 2, 3],
+  ['Ruby OOP', 3, 2, 4],
+  ['SQL in Rails', 4, 2, 5],
+  ['Algo basics', 5, 3, 6],
+  ['Searching', 6, 3, 7]
 ]
 
 tests.each do |title, level, category, user|
