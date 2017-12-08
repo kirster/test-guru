f7febb1d8d698fd0babd1a0e18791639ab02a566
kirster <kir.sazonov@gmail.com>
2017-12-08 17:57:33 +0300

    add css styles via bootstrap

--- Gemfile
+++ Gemfile
@@ -34,6 +34,10 @@ gem 'devise', '~> 4.0'
 
 gem 'rails-i18n', '~> 5.0.0'
 
+gem 'jquery-rails'
+
+gem 'bootstrap', '~> 4.0.0.beta2'
+
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,8 +12,23 @@ module ApplicationHelper
 
   def flash_message
     flash.map do |type, message|
-      content_tag :p, message, class: type
+      content_tag :div, message, class: "#{bootstrap_flash_class(type)}" 
     end.join.html_safe
   end
-  
+
+  def bootstrap_flash_class(type)
+    case type
+      when 'notice' then 'alert alert-info'
+      when 'alert' then 'alert alert-danger'   
+    end
+  end
+
+  def link_to_admin_panel(current_user)
+    if current_user.admin?
+      content_tag :span do
+        concat link_to t('admin_panel'), admin_tests_path, class: "btn btn-primary" 
+      end
+    end
+  end
+
 end
