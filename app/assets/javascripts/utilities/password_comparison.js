4e932dfb72c324d9081c103e7ffd3aed651bb9a8
kirster <kir.sazonov@gmail.com>
2017-12-16 10:01:35 +0300

    fix confimation_password check

--- app/assets/javascripts/utilities/password_comparison.js
+++ app/assets/javascripts/utilities/password_comparison.js
@@ -1,19 +1,22 @@
 document.addEventListener('turbolinks:load', function() {
-  document.getElementById('user_password_confirmation').addEventListener('input', passwordComparison)
+  var pass_confirm = document.getElementById('user_password_confirmation')
+  if (pass_confirm) { pass_confirm.addEventListener('input', passwordComparison) }
 }) 
 
 function passwordComparison() {
   var password = document.getElementById('user_password').value
+  var confirm = this.value
 
-  if (this.value != "" && this.value === password) {
-    if (document.querySelector('.octicon-check').classList.contains('hide')) {
-      document.querySelector('.octicon-check').classList.remove('hide')
+  if (confirm != password.substr(0, confirm.length)) {
+    document.querySelector('.octicon-x').classList.remove('hide')
+    document.querySelector('.octicon-check').classList.add('hide')
+  } else {
       document.querySelector('.octicon-x').classList.add('hide')
-    }
-  } else if (this.value != password && this.value.length != 0 ) {
-      if (document.querySelector('.octicon-x').classList.contains('hide')) {
-        document.querySelector('.octicon-x').classList.remove('hide')
+
+      if (confirm.length == password.length) {
+        document.querySelector('.octicon-check').classList.remove('hide')
+      } else {
         document.querySelector('.octicon-check').classList.add('hide')
-      }
+    }
   }
 }
