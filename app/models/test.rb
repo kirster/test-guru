37e37fbab91ad70450454c83cd981284724a0280
kirster <kir.sazonov@gmail.com>
2018-01-14 09:49:45 +0300

    add timer attribute to test model

--- app/models/test.rb
+++ app/models/test.rb
@@ -16,6 +16,7 @@ class Test < ApplicationRecord
 
   validates :title, presence: true, uniqueness: { scope: :level }
   validates :level, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :timer, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1 } 
 
   def self.tests_by_category(category)
     self.for_category(category).descending
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180111131017) do
+ActiveRecord::Schema.define(version: 20180114062129) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -97,6 +97,7 @@ ActiveRecord::Schema.define(version: 20180111131017) do
     t.datetime "updated_at", null: false
     t.integer "category_id"
     t.integer "user_id"
+    t.integer "timer"
     t.index ["category_id"], name: "index_tests_on_category_id"
     t.index ["level", "title"], name: "index_tests_on_level_and_title", unique: true
     t.index ["user_id"], name: "index_tests_on_user_id"
